2ef85c4f7c9d489b89ea3786771160f1027159a6
Spectro1 <Kevindezcc@gmail.com>
2024-04-11 00:38:11 -0400

    Initial Commit

---
+++ CalculatorButton/CalculatorButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorButtonApp.swift
+//  CalculatorButton
+//
+//  Created by Kevin Hernandez on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
